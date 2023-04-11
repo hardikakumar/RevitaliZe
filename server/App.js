ddc2ceba7a0a5dd0d87ed22614cabf7b7afa4792
hardikakumar <hardikakumar@jklu.edu.in>
2023-04-11 10:58:43 +0530

    Added seperate login navigate for doctor-dashboard with APIs

--- server/App.js
+++ server/App.js
@@ -32,20 +32,17 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 // SIGN-UP FORM
 app.post("/users", async (req, res) => {
-  const { name, age, gender, email,phone, password } = req.body;
+  const { name, age, gender, email, phone, password } = req.body;
   UserModel.findOne({ email: email }).then((users) => {
-      if (users) 
-      {
-        res.status(403).send({message:"User already registered"})
-      }
-      else
-        {
-          UserModel.findOne({ phone: phone }).then((users) => {
-            if (users) 
-            {
-              res.status(403).send({message:"User already registered"})
-            }
-            else{
+    if (users) {
+      res.status(403).send({ message: "User already registered" })
+    }
+    else {
+      UserModel.findOne({ phone: phone }).then((users) => {
+        if (users) {
+          res.status(403).send({ message: "User already registered" })
+        }
+        else {
           let counter = 1;
           encryptedPasscode = sha256(password);
           const user = new UserModel({
@@ -57,11 +54,12 @@ app.post("/users", async (req, res) => {
             encryptedPasscode,
             counter,
           });
-        
+
           user.save();
           console.log("User object:", user);
-          res.send(user);}
-        })
+          res.send(user);
+        }
+      })
     }
   })
     .catch((err) => {
@@ -75,19 +73,15 @@ app.post("/userLogin", async (req, res) => {
   const { email, password } = req.body;
   UserModel.findOne({ email: email })
     .then((users) => {
-      if (users) 
-      {
+      if (users) {
         encryptedPasscode = sha256(password);
-        if (encryptedPasscode === users.encryptedPasscode) 
-        {
+        if (encryptedPasscode === users.encryptedPasscode) {
           res.status(200).send(users);
-        } else 
-        {
+        } else {
           res.status(400).send({ message: "Password didn't match" });
         }
-      } 
-      else 
-      {
+      }
+      else {
         res.status(404).send({ message: "User not registered" });
       }
     })
@@ -120,61 +114,70 @@ app.post("/DailyHealthTips", async (req, res) => {
 // GENERATE DOSHA REPORT & POSTING IT IN DATABASE
 
 app.post("/doshareport", (req, res) => {
-  const { vatta, pitta, kapha, member_id,date } = req.body;
-        const doshareport = new DoshaReportModel({
-          member_id,
-          date,
-          vatta,
-          pitta,
-          kapha,
-        });
-        doshareport.save();
-        //console.log(doshareport);
+  const { vatta, pitta, kapha, member_id, date } = req.body;
+  const doshareport = new DoshaReportModel({
+    member_id,
+    date,
+    vatta,
+    pitta,
+    kapha,
+  });
+  doshareport.save();
+  //console.log(doshareport);
   console.log(doshareport);
 });
 
 
 // FETCH ALL THE PAST DOSHA REPORTS OF THE PARTICULAR USER
-app.post("/latestDoshaScore", async(req, res) => {
+app.post("/latestDoshaScore", async (req, res) => {
   const { member_id } = req.body;
-  const reports = await DoshaReportModel.find({member_id:member_id})
+  const reports = await DoshaReportModel.find({ member_id: member_id })
   console.log(reports);
   const l = reports.length;
-  res.send(reports[l-1]);
- // res.status(200).send(ques);
+  res.send(reports[l - 1]);
+  // res.status(200).send(ques);
 });
 
-app.get("/Dosha", async(req, res) => {
+// FETCHING ALL THE DOSHA REPORTS OF THE PARTICULAR USERS
+app.post("/DoshaReports", async (req, res) => {
+
   const { member_id } = req.body;
-  const reports = await DoshaReportModel.find({member_id:member_id})
+  const reports = await DoshaReportModel.find({ member_id: member_id })
+  res.send(reports);
+
+})
+
+// GET FEATURE TO CHECK THE LATEST 
+app.get("/Dosha", async (req, res) => {
+  const { member_id } = req.body;
+  const reports = await DoshaReportModel.find({ member_id: member_id })
   console.log(reports);
   const l = reports.length;
-  res.send(reports[l-1]);
- // res.status(200).send(ques);
+  res.send(reports[l - 1]);
 });
 
 
 // REMEDIES
 
 app.post("/Remedies", async (req, res) => {
-  const { RemedyName, Description , Eczema, Hyperthyroidism, Hypothyroidism, PCOD, Type, Dosha } = req.body;
-  
-          const remedy = new RemediesModel({
-            RemedyName,
-            Description,
-            Eczema,
-            Hyperthyroidism,
-            Hypothyroidism,
-            PCOD,
-            Type,
-            Dosha
-          });
-        
-          remedy.save();
-          console.log("Remedy object:", remedy);
-          res.send(remedy);
-        })
-        
+  const { RemedyName, Description, Eczema, Hyperthyroidism, Hypothyroidism, PCOD, Type, Dosha } = req.body;
+
+  const remedy = new RemediesModel({
+    RemedyName,
+    Description,
+    Eczema,
+    Hyperthyroidism,
+    Hypothyroidism,
+    PCOD,
+    Type,
+    Dosha
+  });
+
+  remedy.save();
+  console.log("Remedy object:", remedy);
+  res.send(remedy);
+})
+
 
 
 //REMINDERS
@@ -182,26 +185,26 @@ setInterval(() => {
   ReminderModel.find({}).then((reminderList) => {
     if (reminderList) {
       reminderList.forEach(reminder => {
-          const now = new Date()
-          var reminderTime = new Date(reminder.remindAt);
-          if ((reminderTime - now) < 0) {
-              reminderTime.setHours(reminderTime.getHours()+reminder.reminderFreq);
-              ReminderModel.findByIdAndUpdate(reminder._id, { remindAt: reminderTime }).then((remindObj) => {
-              const accountSid = process.env.WHATSAPP_ACCOUNT_SID;
-              const authToken = process.env.WHATSAPP_AUTH_TOKEN;
-              const client = require('twilio')(accountSid, authToken);
+        const now = new Date()
+        var reminderTime = new Date(reminder.remindAt);
+        if ((reminderTime - now) < 0) {
+          reminderTime.setHours(reminderTime.getHours() + reminder.reminderFreq);
+          ReminderModel.findByIdAndUpdate(reminder._id, { remindAt: reminderTime }).then((remindObj) => {
+            const accountSid = process.env.WHATSAPP_ACCOUNT_SID;
+            const authToken = process.env.WHATSAPP_AUTH_TOKEN;
+            const client = require('twilio')(accountSid, authToken);
 
-              client.messages
-                .create({
-                  body: "A reminder from RevitaliZe: *"+reminder.reminderMsg+"*",
-                  from: 'whatsapp:+14155238886',
-                  to: 'whatsapp:+919829081906'
-                  // to: 'whatsapp:+918949291337'
-                }).then(message => console.log(message.sid));
-            }).catch((err) => {
-              console.log(err);
-            });
-          }
+            client.messages
+              .create({
+                body: "A reminder from RevitaliZe: *" + reminder.reminderMsg + "*",
+                from: 'whatsapp:+14155238886',
+                to: 'whatsapp:+919829081906'
+                // to: 'whatsapp:+918949291337'
+              }).then(message => console.log(message.sid));
+          }).catch((err) => {
+            console.log(err);
+          });
+        }
       })
     }
   }).catch((err) => {
@@ -243,6 +246,26 @@ app.post("/deleteReminder", (req, res) => {
       console.log(err);
     });
   })
+})
+
+//FEEDBACKS
+// Feedbacks written by the user
+app.post("/UserFeedbacks", (req, res) => {
+
+  const { member_id, feedback } = req.body;
+  const feedbacks = new FeedbacksModel({
+    member_id,
+    feedback,
+  });
+
+  feedbacks.save();
+  res.send(feedbacks);
+})
+
+// Feedbacks displayed on the doctor panel side
+app.post("/DoctorFeedbacks", async (req, res) => {
+  const feedbacks = await FeedbacksModel.find()
+  res.status(200).send(feedbacks);
 })
 
 app.listen(5000, () => console.log("Backend is running"));
